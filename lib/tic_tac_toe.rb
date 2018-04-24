e0ba62e2a188f162797974915194579cbac1ded9
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-24 22:02:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,8 +26,8 @@ class TicTacToe
     index = input.to_i - 1
   end
 
-  def move(board, index, character)
-      board[index] = character
+  def move(, index, character)
+      @board[index] = character
   end
 
   def position_taken?(board, index)
