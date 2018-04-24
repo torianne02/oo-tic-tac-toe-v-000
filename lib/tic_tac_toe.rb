e3914faaa61326bd6cc890da94755dfd18366a9e
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-24 21:52:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,7 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
-  def WIN_COMBINATIONS = [
+  WIN_COMBINATIONS = [
     [0, 1, 2], # top row
     [3, 4, 5], # middle row
     [6, 7, 8], # bottom row
@@ -12,5 +12,4 @@ class TicTacToe
     [1, 4, 7], # center column
     [2, 5, 8] # right column
   ]
-  end
 end
