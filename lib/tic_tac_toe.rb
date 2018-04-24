6922504820b9fbdbc5a0ca809902060bae95cbbb
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-24 22:04:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,9 +56,9 @@ class TicTacToe
     end
   end
 
-  def turn_count(board)
+  def turn_count
    counter = 0
-   board.each do |turn|
+   @board.each do |turn|
      if turn == "X" || turn == "O"
        counter += 1
      end
